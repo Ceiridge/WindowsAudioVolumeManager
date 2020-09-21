b05d1df7c0d617ee2ccfb6b23662df7bfaa0b87c
Ceiridge <philipp34533@icloud.com>
2020-09-21 21:49:58 +0200

    Enhancement: Added gui, basic functionality

---
+++ AudioSessionElement.cs
@@ -0,0 +1,11 @@
+﻿namespace WindowsAudioVolumeManager {
+	class AudioSessionElement {
+		public string Name { get; set; }
+		public int Volume { get; set; }
+
+		public AudioSessionElement(string name, int volume) {
+			Name = name;
+			Volume = volume;
+		}
+	}
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,78 @@
-﻿using System;
+﻿using CSCore.CoreAudioAPI;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WindowsAudioVolumeManager {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window {
+		private readonly List<MMDevice> Devices = new List<MMDevice>();
+		private readonly ObservableCollection<AudioSessionElement> SessionControls = new ObservableCollection<AudioSessionElement>();
+
 		public MainWindow() {
 			InitializeComponent();
+			SessionListView.DataContext = SessionControls;
+
+			Task.Run(() => { // Windows CoreAudio API has to be called in an MTA thread (typical Microsoft logic)
+				using MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
+				MMDevice defaultDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+				int count = 0;
+
+				foreach (MMDevice device in deviceEnumerator.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active)) {
+					bool isDefault = defaultDevice.DeviceID.Equals(device.DeviceID);
+
+					Invoke(() => {
+						int index = AudioOutputCombo.Items.Add(device.FriendlyName + " (" + device.DeviceID + ")" + (isDefault ? " [PRIMARY]" : ""));
+						Devices.Add(device);
+
+						if (isDefault) {
+							AudioOutputCombo.SelectedIndex = index;
+						}
+					});
+					count++;
+				}
+
+				if(count > 0) {
+					RefreshSessions();
+				}
+			});
+		}
+
+		/// Has to run in an MTA thread
+		private void RefreshSessions() {
+			MMDevice device = Devices[(int)Invoke(() => AudioOutputCombo.SelectedIndex)];
+
+			using AudioSessionManager2 sessionManager = AudioSessionManager2.FromMMDevice(device);
+			using AudioSessionEnumerator sessionEnumerator = sessionManager.GetSessionEnumerator();
+
+			Invoke(SessionControls.Clear);
+			foreach (AudioSessionControl session in sessionEnumerator) {
+				string displayName = session.DisplayName;
+				using AudioSessionControl2 session2 = session.QueryInterface<AudioSessionControl2>();
+				using SimpleAudioVolume volume = session.QueryInterface<SimpleAudioVolume>();
+
+				string name = (displayName == null || displayName.Length == 0) ? session2.Process.MainModule.ModuleName : displayName;
+				int vol = (int)(volume.MasterVolume * 100d);
+
+				Invoke(() => SessionControls.Add(new AudioSessionElement(name, vol)));
+			}
+		}
+
+		private object Invoke(Action action) {
+			return Invoke(() => {
+				action();
+				return null;
+			});
+		}
+		private object Invoke(Func<object> action) {
+			return Dispatcher.Invoke(DispatcherPriority.Normal, action);
+		}
+
+
+		private void RefreshAppsButton_Click(object sender, RoutedEventArgs e) {
+			Task.Run(RefreshSessions);
 		}
 	}
 }
